4dbe4b4b8775dd33202838601b551a3f3153a156
gabriel2m <gabriel2m.contact@gmail.com>
2025-08-20 22:22:59 -0300

    feat: assignment 4

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import status from 'http-status';
+import bodyParser from 'body-parser';
 import path from './helpers/path.js';
 import routes from './routes.js';
-import status from 'http-status';
 
 express()
+    .set('view engine', 'pug')
     .use(express.static(path.base('public')))
+    .use(bodyParser.urlencoded())
     .use(routes)
     .use((_request, response) => response.sendStatus(status.NOT_FOUND))
     .listen(process.env.APP_PORT);
--- routes.js
+++ routes.js
@@ -1,6 +1,13 @@
 import { Router } from 'express';
-import path from './helpers/path.js';
+
+const data = {
+    users: []
+};
 
 export default Router()
-    .get('/users', (_request, response) => response.sendFile(path.view('users')))
-    .get('/', (_request, response) => response.sendFile(path.view('home')));
+    .get('/users', (_request, response) => response.render('users', data))
+    .post('/users', (request, response) => {
+        data.users.push(request.body);
+        response.redirect('/users');
+    })
+    .get('/', (_request, response) => response.render('home'));
